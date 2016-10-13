4cf04b15822e313fa9055023a00e3fda236a0577
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-10-13 14:18:25 +0200

    Removed ReactDOM from frontpage.js

--- web/bundle.js
+++ web/bundle.js
@@ -27395,7 +27395,6 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var React = __webpack_require__(1);
-	var ReactDOM = __webpack_require__(34);
 	var ReactRouter = __webpack_require__(172);
 
 	var showPost = false;
