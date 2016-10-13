f9e37e25520f07f1acd8b25914ee5185852c9b43
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-10-13 17:08:24 +0200

    Added posts.js

---
+++ react/posts.js
@@ -0,0 +1,6 @@
+var posts = [
+  {id: "post_hash1", title: "Ironing", description: "I can't for the life of me find out how to iron a shirt?", time_posted: "11. jan", author: "bob poopmaster", comments: 1},
+  {id: "post_hash2", title: "Parenting", description: "How to I wipe my kids snot, and how do I parent?", time_posted: "12. jan", author: "mongo", comments: 20}
+];
+
+module.exports = posts;
