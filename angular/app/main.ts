eda10b76cb26caecbaa3a1fbdb615373bcd6acbf
agnete <agnetedjupvik@gmail.com>
2016-10-20 15:21:20 +0200

    Document main.ts

--- angular/app/main.ts
+++ angular/app/main.ts
@@ -1,4 +1,4 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; /*Lets us use Angular in our web app*/
+import { AppModule } from './app.module'; /*Here, we connect AppModule which loads the entire app*/
 const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+platform.bootstrapModule(AppModule); /*Launches the application*/
