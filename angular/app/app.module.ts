f7f7969aed4e52fe5b697d1cc5d856f49c2fb495
Knut Fludal <knutfludal@gmail.com>
2016-10-20 15:31:28 +0200

    Comments in module and routing

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -7,6 +7,14 @@ import { HomeComponent }	  from './components/home/home.component';
 import { AppComponent }   from './app.component';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
+
+
+/*	Imports:  list of external modules we import
+	Declarations: list of modules we use internally.
+	Bootstrap: Declare what to start when the module starts. In this case, just the app component. 
+	Directives: Used for changing the content of views. We only use ROUTER_DIRECTIVES for this. 
+
+ */
 @NgModule({
     imports: [BrowserModule,routing], 
     declarations: [AppComponent,ProfileComponent, HomeComponent],
