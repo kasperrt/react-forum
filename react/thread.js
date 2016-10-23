197ce9e992700ce26d53f50e85928b0f721f55b6
Masoom Maham <masoommaham@gmail.com>
2016-10-23 12:26:52 +0200

    Added comments to thread.js

--- react/thread.js
+++ react/thread.js
@@ -1,8 +1,8 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 
-var Thread = React.createClass({
-  render: function() {
+var Thread = React.createClass({  //Creates a new class, which contains the post, comments, and the new comment button and textarea
+  render: function() {            //rendering function, returns what to be displayed
     var id = this.props.hash;
     var posts = this.props.posts;
     var post = posts.filter(function( obj ) {
@@ -12,26 +12,26 @@ var Thread = React.createClass({
     return (
       <div>
         <div className="postContainer">
-          <h1>{post.title}</h1>
+          <h1>{post.title}</h1> //gets the title value for the post
           <div className="postDescriptionContainer">
-            {post.description}
+            {post.description} //gets the description value for the post
           </div>
           <div className="postInformation">
             <span>
-              Skrevet av <ReactRouter.Link to={"user/"+post.author}>
-                {post.author}
+              Skrevet av <ReactRouter.Link to={"user/"+post.author}> //links to the author's profile page
+                {post.author} //gets the author value for the post
               </ReactRouter.Link><br/>
             </span>
             <span>
-              {post.comments.length} kommentarer<br/>
+              {post.comments.length} kommentarer<br/> //gets the amount of commetns a post has
             </span>
-            Publisert {post.time_posted}
+            Publisert {post.time_posted} //gets the time_posted value for the post
           </div>
         </div>
         <hr/>
         <div className="commentSorter">
           Sorter på
-          <select>
+          <select> //selecter to decide how the comments should be sorted, no functionality yet
             <option value="nyeste">nyeste først</option>
             <option value="eldste">eldste først</option>
           </select>
@@ -41,13 +41,13 @@ var Thread = React.createClass({
             <textarea rows="10" cols="80" placeholder="Skriv inn kommentar her" className="newCommentTextArea">
             </textarea>
           </div>
-          <ReactRouter.Link className="newCommentButton" to={"createComment"}>
+          <ReactRouter.Link className="newCommentButton" to={"createComment"}> //Links to a create comment page, no functionality yet
               Publiser
           </ReactRouter.Link>
         </div>
         <div className="thread_comments">
           <Comments comments={post.comments}>
-            {this.props.children}
+            {this.props.children} //Renders the children sent from the Comments class
           </Comments>
         </div>
       </div>
@@ -55,25 +55,26 @@ var Thread = React.createClass({
   }
 });
 
+//Comments class
 var Comments = React.createClass({
   render: function(){
-    var commentComponents = this.props.comments.map(function(comment){
+    var commentComponents = this.props.comments.map(function(comment){      //Loops through comments and applies design for every comment on the page
       return (
         <div id={comment.id} key={comment.id} className="comment">
-          <ReactRouter.Link className="commentAuthor" to={"user/" + comment.author}>
-          {comment.author}
+          <ReactRouter.Link className="commentAuthor" to={"user/" + comment.author}>  //links to the author's profile page
+          {comment.author}  //gets the author value for the comment
           </ReactRouter.Link>
           <div className={comment.comment_text}>
-            {comment.comment_text}
+            {comment.comment_text} //gets the comment_text value for the comment
           </div>
           <div className="commentPosted">
-            Publisert {comment.time_posted}
+            Publisert {comment.time_posted} //gets the time_posted value for the comment
           </div>
         </div>
       );
     });
-    return <div> {commentComponents} </div>;
+    return <div> {commentComponents} </div>; //returns all of the comments for a post
   }
 })
 
-module.exports = Thread;
+module.exports = Thread;  //exports Thread variable to any other class/file that use require('./thread.js');
