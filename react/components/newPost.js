723294f4396a6c3c888d1fd1338b2e2e46bc55c6
agnete <agnetedjupvik@gmail.com>
2016-11-02 14:29:07 +0100

    Style new post button with flexbox

--- react/components/newPost.js
+++ react/components/newPost.js
@@ -3,13 +3,15 @@ import React from 'react';
 const NewPost = ({posted, showPost, addNewPost, handleTitleChange, handleDescriptionChange}) => (
   <div id="new_post">
     {showPost && !posted ?
-      <div className="new_post before_posted">
-      <input type="text" name="title" placeholder="Title of post" onChange={handleTitleChange} />
-      <textarea className="new_post_text" placeholder="Description.." onChange={handleDescriptionChange}>
-      </textarea>
-    </div>
-    : null}
-    {posted ? <div className="success {!posted ? 'hidden' : ''}">Posted</div> : null }
+      <div className="new_post">
+        <input type="text" name="title" placeholder="Title of post" onChange={handleTitleChange} />
+        <textarea className="new_post_text" placeholder="Description.." onChange={handleDescriptionChange}>
+        </textarea>
+      </div>
+      : null}
+    {posted ?
+      <div className="success {!posted ? 'hidden' : ''}">Posted</div>
+      : null }
     <a href="#" className="new_post_button { showButton ? 'hidden' : '' }" onClick={() => addNewPost()}>Nytt Innlegg</a>
   </div>
 );
