d152451fa87001956fe625c40a1e18e201f90953
agnete <agnetedjupvik@gmail.com>
2016-11-09 11:03:26 +0100

    Replace pagination text with arrows

--- react/components/PageNavigation.js
+++ react/components/PageNavigation.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 const PageNavigation = ({currentPage, previousPage, nextPage, morePages}) => (
   <div className="navigation_div">
-    {currentPage > 1 ? <button onClick={() => previousPage()}>
-      previous page
-      </button> : null}
+    {currentPage > 1 ?
+      <img onClick={() => previousPage()} src="http://image.flaticon.com/icons/svg/60/60573.svg" alt="Next page" height="10px"></img>
+      : null}
     page {currentPage}
     {morePages ?
-      <button onClick={() => nextPage()}>
-        next page
-      </button>
+      <img onClick={() => nextPage()} src="http://image.flaticon.com/icons/svg/60/60758.svg" alt="Next page" height="10px"></img>
     : null}
   </div>
 );
