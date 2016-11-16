0d31a5d717a4e79365cebb46f1aab27eb77b8888
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-11-16 12:08:26 +0100

    Updated some functions

--- react/components/Sorting.js
+++ react/components/Sorting.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Sorting = ({type, way, handleTypeChange, handleWayChange, type_hide}) => (
   <div className="sorting_container">
-      Sorter på
+      <span>Sorter på</span>
       { !type_hide ? <select value={type} onChange={handleTypeChange}>
         <option value="date">dato</option>
         <option value="title">tittel</option>
       </select>
       : " dato " }
       <select value={way} onChange={handleWayChange}>
-        <option value="desc">desc</option>
-        <option value="asc">asc</option>
+        <option value="desc">synkende</option>
+        <option value="asc">stigende</option>
       </select>
   </div>
 );
